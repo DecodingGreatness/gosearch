24edbb4f40af17154517d3db12274b9d75884061
Edward Ho <pythoned77@gmail.com>
2022-09-07 15:34:01 +0800

    added paths to specific routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./index.css";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
-import { Routes } from "./components/Routes";
+import { Paths } from "./components/Paths";
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
@@ -10,7 +10,7 @@ const App = () => {
     <div className={darkTheme ? "dark" : ""}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
         <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
-        <Routes />
+        <Paths />
         <Footer />
       </div>
     </div>
