d931a8666f54de1db4beae066811499f31a9c2d4
Edward Ho <pythoned77@gmail.com>
2022-09-07 14:40:37 +0800

    added mode button and updated tailwind darkmode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, usestate } from "react";
+import React, { useState } from "react";
 import "./index.css";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div className={darkTheme ? "dark" : ""}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
-        <Navbar />
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
         <Routes />
         <Footer />
       </div>
