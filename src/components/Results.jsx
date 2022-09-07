b6bd9527094489c8f7b9999a4b0d8cf90abd530c
Edward Ho <pythoned77@gmail.com>
2022-09-07 16:46:46 +0800

    added swtich for location pathname for Results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactPlayer from "react-player";
+
+import { useResultContext } from "../contexts/ResultContextProvider";
+import { Loading } from "./Loading";
 
 export const Results = () => {
+  const { results, isLoading, getResults, searchTerm } = useResultContext();
+  const location = useLocation();
+
+  if (isLoading) return <Loading />;
+
+  switch (location.pathname) {
+    case "/search":
+      return "SEARCH";
+
+    case "/images":
+      return "SEARCH";
+
+    case "/news":
+      return "SEARCH";
+
+    case "/news":
+      return "SEARCH";
+
+    default:
+      return "ERROR";
+  }
+
   return <div>Results</div>;
 };
--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "React";
+import React, { createContext, useContext, useState } from "react";
 
 const ResultContext = createContext();
 const baseURL = "https://google-search1.p.rapidapi.com/google-search";
@@ -13,7 +13,26 @@ export const ResultContextProvider = ({ children }) => {
 
     const response = await fetch(`${baseURL}${type}`, {
       method: "GET",
-      headers: {},
+      headers: {
+        "x-user-agent": "desktop",
+        "x-rapidapi-host": process.env.REACT_APP_RAPIDAPI_HOST,
+        "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
+      },
     });
+
+    const data = await response.json();
+
+    setResults(data);
+    setIsLoading(false);
   };
+
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
 };
+
+export const useResultContext = () => useContext(ResultContext);
