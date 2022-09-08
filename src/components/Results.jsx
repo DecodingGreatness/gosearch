599f0b0379f63ec777e483d402c3eca02b14399f
Edward Ho <pythoned77@gmail.com>
2022-09-08 12:30:22 +0800

    changed images to image for both results.jsx and paths.jsx -> now images url is working

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -6,12 +6,23 @@ import { useResultContext } from "../contexts/ResultContextProvider";
 import { Loading } from "./Loading";
 
 export const Results = () => {
-  const { results, isLoading, getResults, searchTerm } = useResultContext();
+  const {
+    results: { results, image_results },
+    isLoading,
+    getResults,
+    searchTerm,
+  } = useResultContext();
   const location = useLocation();
 
   useEffect(() => {
-    getResults(`/search/q=elon+musk`);
-  }, []);
+    if (searchTerm) {
+      if (location.pathname === "/videos") {
+        getResults(`/search/q=${searchTerm} videos`);
+      } else {
+        getResults(`${location.pathname}/q=${searchTerm}&num=40`);
+      }
+    }
+  }, [searchTerm, location.pathname]);
 
   if (isLoading) return <Loading />;
 
@@ -19,11 +30,11 @@ export const Results = () => {
     case "/search":
       return (
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
-          {results?.results?.map(({ url, title }, index) => (
+          {results?.map(({ link, title }, index) => (
             <div key={index} className="md:w-2/5 w-full">
-              <a href={url} target="_blank" rel="noreferrer">
+              <a href={link} target="_blank" rel="noreferrer">
                 <p className="text-sm">
-                  {/* {url.length > 30 ? url.substring(0, 30) : url} */}
+                  {link.length > 30 ? link.substring(0, 30) : link}
                 </p>
                 <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
                   {title}
@@ -34,11 +45,48 @@ export const Results = () => {
         </div>
       );
 
-    case "/images":
-      return "SEARCH";
+    case "/image":
+      return (
+        <div className="flex flex-wrap justify-center items-center">
+          {image_results?.map(({ image, link: { href, title } }, index) => (
+            <a
+              href={href}
+              target="_blank"
+              key={index}
+              rel="noreferrer"
+              className="sm:p-3 p-5"
+            >
+              <img src={image?.src} alt={title} loading="lazy" />
+              <p className="sm:w-36 w-36 break-words text-sm mt-2">{title}</p>
+            </a>
+          ))}
+        </div>
+      );
 
-    case "/news":
-      return "SEARCH";
+    // case "/news":
+    //   return (
+    //     <div className="flex flex-wrap justify-between space-y-6 sm:px-56 items-center">
+    //       {news?.map(({ links, id, source, title }) => (
+    //         <div key={id} className="md:w-2/5 w-full">
+    //           <a
+    //             href={links?.[0].href}
+    //             target="_blank"
+    //             rel="noreferrer"
+    //             className="hover:underline"
+    //           >
+    //             <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+    //               {title}
+    //             </p>
+    //             <div className="flex gap-4">
+    //               <a href={source?.href} target="_blank" rel="noreferrer">
+    //                 {source?.href}
+    //               </a>
+    //             </div>
+    //           </a>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   );
 
     case "/videos":
       return "SEARCH";
